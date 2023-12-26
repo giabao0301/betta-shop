956ca005b0b32ce094d577f19a8722ae7a9a53ee
NguyenKiet-Anh <anhkiet.nguyen798@gmail.com>
2023-12-26 15:07:48 +0700

    signup changes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router'
+
 function Signup() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [fullname, setFullname] = useState('');
+    const [email, setEmail] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSignup = async () => {
+        try {
+          const response = await fetch('http://localhost:8000/signup/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password,
+              fullname: fullname,
+              email: email
+            }),
+          });
+    
+          if (response.ok) {
+            const data = await response.json();
+            if (data.success){
+                alert('Signup Successfully!');
+                // Going to 'Trang chủ'
+                navigate('/')
+            }                
+            else
+                {
+                    alert('Signup failed!')
+                }
+          } else {
+            alert('Signup failed!');
+          }
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+
     return (
         <section className="h-screen flex items-center mt-8">
             <div className="w-full lg:w-4/12 px-4 mx-auto pt-6">
@@ -13,12 +57,14 @@ function Signup() {
                                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                                     htmlFor="grid-password"
                                 >
-                                    Email
+                                    USERNAME
                                 </label>
                                 <input
-                                    type="email"
+                                    type="text"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
                                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                    placeholder="Email"
+                                    placeholder="Username"
                                 />
                             </div>
                             <div className="relative w-full mb-3">
@@ -30,6 +76,8 @@ function Signup() {
                                 </label>
                                 <input
                                     type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
                                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                                     placeholder="Password"
                                 />
@@ -39,12 +87,42 @@ function Signup() {
                                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                                     htmlFor="grid-password"
                                 >
-                                    Nhập lại mật khẩu
+                                    RE-ENTER PASSWORD
                                 </label>
                                 <input
                                     type="password"
                                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                                     placeholder="Password"
+                                />
+                            </div>
+                            <div className="relative w-full mb-3">
+                                <label
+                                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                                    htmlFor="grid-password"
+                                >
+                                    FULL NAME
+                                </label>
+                                <input
+                                    type="text"
+                                    value={fullname}
+                                    onChange={(e) => setFullname(e.target.value)}
+                                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                    placeholder="Your full name"
+                                />
+                            </div>
+                            <div className="relative w-full mb-3">
+                                <label
+                                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                                    htmlFor="grid-password"
+                                >
+                                    Email
+                                </label>
+                                <input
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                    placeholder="Email"
                                 />
                             </div>
                             <div>
@@ -65,6 +143,7 @@ function Signup() {
                                 <button
                                     className="bg-primary text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                                     type="button"
+                                    onClick={handleSignup}
                                 >
                                     Đăng ký
                                 </button>
