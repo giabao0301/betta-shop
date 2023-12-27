bedcccb9111227a7dc32bf615b36b37f94d3ea6f
NguyenKiet-Anh <anhkiet.nguyen798@gmail.com>
2023-12-27 21:13:41 +0700

    using fetched data

--- src/components/Layout/Content/ProductDisplay.jsx
+++ src/components/Layout/Content/ProductDisplay.jsx
@@ -5,13 +5,13 @@ import useCartContext from '../../../store/cart-context';
 
 const ProductDisplay = (props) => {
     const { product } = props;
-    const new_price = `${product.new_price},000`;
-    const old_price = `${product.old_price},000`;
+    const new_price = product ? `${product.gia / 1000},000` : '';
+    const old_price = product ? `${(product.gia / 1000) + 50},000` : '';
 
     const [quantity, setQuantity] = useState(1);
     const amountInputRef = useRef();
     const { addItem } = useCartContext();
-    console.log('re-render');
+    
     const descreaseQuantityHandler = () => {
         setQuantity((prevState) => prevState > 1 && prevState - 1);
     };
@@ -25,36 +25,51 @@ const ProductDisplay = (props) => {
         const enteredAmountNumber = +enteredAmount;
         addItem({ ...product, amount: enteredAmountNumber });
     };
+
+    const getFullImagePath = (relativePath) => {
+        const baseUrl = 'http://127.0.0.1:8000'; // Replace with your Django base URL
+        var ImageUrl = `${baseUrl}${relativePath}`
+        ImageUrl = ImageUrl.replace('/media/', '/static/')
+        return ImageUrl;
+      };
+    
     return (
         <section className="overflow-hidden bg-white">
             <div className="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
                 <div className="flex flex-wrap -mx-4">
                     <div className="w-full px-4 md:w-1/2 ">
                         <div className="top-0 z-50 overflow-hidden flex gap-5 h-full">
-                            <div className="flex-wrap hidden md:flex flex-col w-1/4">
-                                <a href="#" className="block border opacity-50 hover:opacity-50">
-                                    <img src={product.image} alt="" className="object-cover" />
-                                </a>
-                                <a href="#" className="block border hover:opacity-50">
-                                    <img src={product.image} alt="" className="object-cover w-full" />
-                                </a>
-                                <a href="#" className="block border  hover:opacity-50">
-                                    <img src={product.image} alt="" className="object-cover w-full" />
-                                </a>
-                                <a href="#" className="block border  hover:opacity-50">
-                                    <img src={product.image} alt="" className="object-cover w-full" />
-                                </a>
-                            </div>
+                            {product && product.hinh_anh1 ? (
+                                <div className="flex-wrap hidden md:flex flex-col w-1/4">
+                                    <a href="#" className="block border opacity-50 hover:opacity-50">
+                                        <img src={getFullImagePath(product.hinh_anh1)} alt="" className="object-cover" />
+                                    </a>
+                                    <a href="#" className="block border hover:opacity-50">
+                                        <img src={getFullImagePath(product.hinh_anh2)} alt="" className="object-cover w-full" />
+                                    </a>
+                                    <a href="#" className="block border hover:opacity-50">
+                                        <img src={getFullImagePath(product.hinh_anh3)} alt="" className="object-cover w-full" />
+                                    </a>
+                                    <a href="#" className="block border hover:opacity-50">
+                                        <img src={getFullImagePath(product.hinh_anh4)} alt="" className="object-cover w-full" />
+                                    </a>
+                                </div>
+                            ) : (
+                                <p>Loading...</p>
+                            )}
                             <div className="relative mb-6 lg:mb-10 w-full">
-                                <img src={product.image} alt="" className="object-cover w-full" />
+                                {product && product.hinh_anh1 ? (
+                                    <img src={getFullImagePath(product.hinh_anh1)} alt="" className="object-cover w-full" />
+                                ) : (
+                                    <p>Loading...</p>
+                                )}
                             </div>
                         </div>
                     </div>
                     <div className="w-full px-4 md:w-1/2 ">
                         <div className="lg:pl-10">
                             <div className="">
-                                <h2 className="max-w-xl mt-2 mb-6 text-2xl font-bold md:text-4xl">{product.name}</h2>
-
+                                <h2 className="max-w-xl mt-2 mb-6 text-2xl font-bold md:text-4xl"> {product && product.ten_ca ? product.ten_ca : "Loading..."} </h2>
                                 <p className="inline-block mb-8 text-4xl font-bold text-dark ">
                                     <span className="mr-2">
                                         {new_price}
