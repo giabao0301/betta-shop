e62263c708b6c742c05f31f61da7e3d14d5035a7
NguyenKiet-Anh <anhkiet.nguyen798@gmail.com>
2023-12-27 21:00:26 +0700

    change

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Item from '../components/BettaFish/BettaFishItem';
 import Button from '../components/UI/Button';
 import { FaAngleDown } from 'react-icons/fa6';
-import all_product from '../assets/all_product';
+import fetchDataFromDjango_Data_Category from '../assets/all_product';
 
 function Category(props) {
+    const [dataProduct, setDataProduct] = useState([]);
+    const { productId } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchDataFromDjango_Data_Category();
+        setDataProduct(data);
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+    const getFullImagePath = (relativePath) => {
+        const baseUrl = 'http://127.0.0.1:8000'; // Replace with your Django base URL
+        var ImageUrl = `${baseUrl}${relativePath}`
+        ImageUrl = ImageUrl.replace('/media/', '/static/')
+        return ImageUrl;
+    };
+
     return (
         <div className="md:p-10 p-2.5 lg:mt-[80px] mt-[64px]">
             <div className="flex mx-44 my-5 justify-between items-center">
@@ -22,16 +47,16 @@ function Category(props) {
                 </div>
             </div>
             <div className="w-fit mx-auto grid grid-cols-2 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center md:gap-y-20 gap-y-6 md:gap-x-14 gap-x-4">
-                {all_product.map((item) => {
-                    if (props.category === item.category) {
+                {dataProduct.map((item) => {
+                    if (props.category === item.ma_loai_ca_info.ten_loai_ca.toLowerCase()) {
                         return (
                             <Item
-                                key={item.id}
-                                id={item.id}
-                                name={item.name}
-                                image={item.image}
-                                new_price={item.new_price}
-                                old_price={item.old_price}
+                                key={item.ma_ca}
+                                id={item.ma_ca}
+                                name={item.ten_ca}
+                                image={getFullImagePath(item.hinh_anh1)}
+                                new_price={item.gia/1000}
+                                old_price={(item.gia + 50) / 1000}
                             />
                         );
                     } else {
