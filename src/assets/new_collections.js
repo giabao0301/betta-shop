ffee74c56ec3dcda79e1ab45914113c507205f78
NguyenKiet-Anh <anhkiet.nguyen798@gmail.com>
2023-12-27 09:36:51 +0700

    using dynamic data instead of static data

--- src/assets/new_collections.js
+++ src/assets/new_collections.js
@@ -1,62 +1,14 @@
-import img_8486 from './images/8486.jpg';
-
-let new_collections = [
-    {
-        id: 1,
-        name: 'Koi nemo candy nền vàng cam đẹp Eileithyia',
-        image: img_8486,
-        new_price: 140,
-        old_price: 180,
-    },
-    {
-        id: 2,
-        name: 'Koi nemo candy nền vàng cam đẹp Eileithyia',
-        image: img_8486,
-        new_price: 140,
-        old_price: 180,
-    },
-    {
-        id: 3,
-        name: 'Koi nemo candy nền vàng cam đẹp Eileithyia',
-        image: img_8486,
-        new_price: 140,
-        old_price: 180,
-    },
-    {
-        id: 4,
-        name: 'Koi nemo candy nền vàng cam đẹp Eileithyia',
-        image: img_8486,
-        new_price: 140,
-        old_price: 180,
-    },
-    {
-        id: 5,
-        name: 'Koi nemo candy nền vàng cam đẹp Eileithyia',
-        image: img_8486,
-        new_price: 140,
-        old_price: 180,
-    },
-    {
-        id: 6,
-        name: 'Koi nemo candy nền vàng cam đẹp Eileithyia',
-        image: img_8486,
-        new_price: 140,
-        old_price: 180,
-    },
-    {
-        id: 7,
-        name: 'Koi nemo candy nền vàng cam đẹp Eileithyia',
-        image: img_8486,
-        new_price: 140,
-        old_price: 180,
-    },
-    {
-        id: 8,
-        name: 'Koi nemo candy nền vàng cam đẹp Eileithyia',
-        image: img_8486,
-        new_price: 140,
-        old_price: 180,
-    },
-];
-
-export default new_collections;
+const fetchDataFromDjango = async () => {
+    try {
+      const response = await fetch('http://192.168.68.102:8000/getfish_special/');  // Adjust the URL based on your Django project structure
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
+  export default fetchDataFromDjango;
+  
