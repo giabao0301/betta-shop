870a1fbc6f6d0d2230e22da8e617957c73c0c5b4
NguyenKiet-Anh <anhkiet.nguyen798@gmail.com>
2023-12-27 21:01:02 +0700

    change

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Breadcrumb from '../components/Layout/Content/Breadcrumb';
 import ProductDisplay from '../components/Layout/Content/ProductDisplay';
 import RelatedProducts from '../components/Layout/Content/RelatedProducts';
-import all_product from '../assets/all_product';
+import fetchDataFromDjango_Data_Category from '../assets/all_product';
 
 function Product() {
+    const [dataProduct, setDataProduct] = useState([]);
     const { productId } = useParams();
-    const product = all_product.find((item) => item.id === Number(productId));
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchDataFromDjango_Data_Category();
+        setDataProduct(data);
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+    
+    const product = dataProduct.find((item) => item.ma_ca === Number(productId));
     return (
         <div className="mt-navbarHeight">
             <Breadcrumb product={product} />
